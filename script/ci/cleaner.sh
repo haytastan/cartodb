daaee177da773abc54ad0d96e89f72cebd8d8b01
albertoh <albertoh@cartodb.com>
2020-01-07 15:17:01 +0100

    changing to parallel tests

--- script/ci/cleaner.sh
+++ script/ci/cleaner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+dbServer="postgres-server"
 # Kill redis
 killall redis-server
 # Drop all databases
-databases=$(psql -U postgres -t -c "select datname from pg_database where datname like 'carto_db_test_%'")
+databases=$(psql -h $dbServer -U postgres -t -c "select datname from pg_database where datname like 'carto_db_test_%'")
 touch parallel_tests/databases.log
 echo $databases >> parallel_tests/databases.log
 touch parallel_tests/databases_new.log
@@ -12,12 +12,12 @@ sed -e 's/\s\+/\n/g' parallel_tests/databases.log > parallel_tests/databases_new
 while read -r line
 do
   [ -z "$line" ] && continue
-  psql -U postgres -t -c "drop database $line" >> parallel_tests/cleaner.log
+  psql -h $dbServer  -U postgres -t -c "drop database $line" >> parallel_tests/cleaner.log
 done < parallel_tests/databases_new.log
 
 
 # Drop all user databases
-databases=$(psql -U postgres -t -c "select datname from pg_database where datname like 'cartodb_test_user_%'")
+databases=$(psql -h $dbServer  -U postgres -t -c "select datname from pg_database where datname like 'cartodb_test_user_%'")
 touch parallel_tests/user_databases.log
 echo $databases >> parallel_tests/user_databases.log
 touch parallel_tests/user_databases_new.log
@@ -26,14 +26,14 @@ sed -e 's/\s\+/\n/g' parallel_tests/user_databases.log > parallel_tests/user_dat
 while read -r line
 do
   [ -z "$line" ] && continue
-  psql -U postgres -t -c "drop database \"$line\"" >> parallel_tests/cleaner.log
+  psql -h $dbServer  -U postgres -t -c "drop database \"$line\"" >> parallel_tests/cleaner.log
 done < parallel_tests/user_databases_new.log
 
 rm parallel_tests/user_databases.log
 rm parallel_tests/users_databases_new.log
 
 # Drop all testing databases
-databases=$(psql -U postgres -t -c "select datname from pg_database where datname like 'cartodb_user_%'")
+databases=$(psql -h $dbServer  -U postgres -t -c "select datname from pg_database where datname like 'cartodb_user_%'")
 touch parallel_tests/user_databases.log
 echo $databases >> parallel_tests/user_databases.log
 touch parallel_tests/user_databases_new.log
@@ -42,7 +42,7 @@ sed -e 's/\s\+/\n/g' parallel_tests/user_databases.log > parallel_tests/user_dat
 while read -r line
 do
   [ -z "$line" ] && continue
-  psql -U postgres -t -c "drop database \"$line\"" >> parallel_tests/cleaner.log
+  psql -h $dbServer -U postgres -t -c "drop database \"$line\"" >> parallel_tests/cleaner.log
 done < parallel_tests/user_databases_new.log
 
 # Cleanup
